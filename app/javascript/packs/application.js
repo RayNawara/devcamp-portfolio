c849259d64e91dc37c0ffc99f61a066cb1fb4e70
RayNawara <rjnawara@gmail.com>
2020-03-14 15:13:49 -0400

    Installed jQuery ui, sortable html5 library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require("jquery");
+require("packs/html5sortable");
 
 import "bootstrap";
 import "./src/application.scss";
